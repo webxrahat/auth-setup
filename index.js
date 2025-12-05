7f7b31e39821ed1ccb4edba6948f2af5655abc30
webxrahat <islammdrahat308@gmail.com>
2025-12-05 21:16:08 +0600

    auth setup

---
+++ index.js
@@ -0,0 +1,35 @@
+import express from "express";
+const app = express();
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import session from "express-session";
+import bcrypt from "bcryptjs";
+import { connectDB } from "./config/connetDB.js";
+
+
+
+
+dotenv.config();
+connectDB();
+
+app.use(express.json());
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET || "defaultsecret",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false },
+  })
+);
+
+
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
