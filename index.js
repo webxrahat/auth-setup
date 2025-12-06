0633b1f0b3b9f4f1c420f2013c0de2680389b302
webxrahat <islammdrahat308@gmail.com>
2025-12-06 08:10:35 +0600

    error handler

--- index.js
+++ index.js
@@ -1,32 +1,44 @@
 import express from "express";
 const app = express();
 import dotenv from "dotenv";
-import mongoose from "mongoose";
 import session from "express-session";
 import bcrypt from "bcryptjs";
 import { connectDB } from "./config/connetDB.js";
-
-
-
+import User from "./models/user.model.js";
 
 dotenv.config();
 connectDB();
 
 app.use(express.json());
-app.use(
-  session({
-    secret: process.env.SESSION_SECRET || "defaultsecret",
-    resave: false,
-    saveUninitialized: true,
-    cookie: { maxAge: Number(process.env.MAXAGE),  secure: false },
-  })
-);
+// app.use(
+//   session({
+//     secret: process.env.SESSION_SECRET || "defaultsecret",
+//     resave: false,
+//     saveUninitialized: true,
+//     cookie: { maxAge: Number(process.env.MAXAGE),  secure: false },
+//   })
+// );
 
 
 
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
+app.get("/api", (req, res) => {
+  res.send("Hello, World!");
+});
+app.post("/login", (req, res) => {
+  res.send("Hello, World!");
+});
+app.post("/register", (req, res) => {
+  const { username, password } = req.body;
+  const hashedPassword = bcrypt.hashSync(password, 10);
+  res.send({ username, hashedPassword });
+  // const newUser = new User({ username, password: hashedPassword });
+  // newUser.save()
+  //   .then(() => res.status(201).send("User registered"))
+  //   .catch((err) => res.status(500).send("Error registering user"));
+});
+app.get("/profile", (req, res) => {
   res.send("Hello, World!");
 });
 
